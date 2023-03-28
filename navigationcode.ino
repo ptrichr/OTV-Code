ee0fb19e4d33e04378df3d07cdcfb7a02b4d5f95
NH <70962997+petrichor-petrichor@users.noreply.github.com>
2023-03-28 12:14:05 -0400

    Add files via upload

---
+++ navigationcode.ino
@@ -0,0 +1,14 @@
+#include <Enes100.h>
+Enes100.begin()
+float xpos = Enes100.location.x, ypos = Enes100.location.y, apos = Enes100.location.theta
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
