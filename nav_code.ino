888dd62882c0825647c0f3e17985890d1a5c1a0f
Jeremy Yun <113321384+JermYeWorm@users.noreply.github.com>
2023-05-09 20:14:15 -0400

    Add files via upload

--- nav_code.ino
+++ nav_code.ino
@@ -1,17 +1,59 @@
-#include "nav_code.h"
+#include <math.h>
+#include "Enes100.h"
 #include <Servo.h>
 
 /*
 * DISCLAIMER: We used GPT-4 to generate the code that the distance sensor uses to read distance (getDistance)
 */
 
+const double NORTH = M_PI/2;
+const double SOUTH = -(double)M_PI/2;
+const double EAST = 0;
+const double WEST = M_PI;
+const double MARGIN = M_PI/10;
+
+// pins for the distance sensors (PWM)
+
+// left
+const int TRIGGER_1 = 4;
+const int ECHO_1 = 5;
+
+// right
+const int TRIGGER_2 = 6;
+const int ECHO_2 = 7;
+
+// pins for motor control (PWM)
+
+// left motor
+const int EN_A = 8;
+const int IN_1 = 9;
+const int IN_2 = 10;
+
+// right motor
+const int EN_B = 13;
+const int IN_3 = 11;
+const int IN_4 = 12;
+
+// Create the servo object
 Servo myservo;
+
+// Topology sensors
+const uint8_t sensorPinLeft = 30;
+const uint8_t sensorPinRight = 31;
+
+// Flame sensors
+int analogPinIR1 = A0;
+int analogPinIR2 = A1;
+int analogPinIR3 = A2;
+int analogPinIR4 = A3;
 int analogIRVal; //analog readings
 
+
+
+/* The code inside void setup() runs only once, before the code in void loop(). */
 void setup() {
- 
-  // param: aruco, tx, rx
-  missionStart(206, 51, 50);
+
+	Enes100.begin("Trial By Fire", FIRE, 206, 51, 50); // param: name, mission, aruco, tx, rx
   Serial.begin(9600); // DO NOT CHANGE, ESP8266 BAUD IS 9600
 
   pinMode(EN_A, OUTPUT);
@@ -24,33 +66,28 @@ void setup() {
   pinMode(TRIGGER_2, OUTPUT);
   pinMode(ECHO_1, INPUT);
   pinMode(ECHO_2, INPUT);
+
   pinMode(analogPinIR1, INPUT);
   pinMode(analogPinIR2, INPUT);
   pinMode(analogPinIR3, INPUT);
   pinMode(analogPinIR4, INPUT);
 
-  myservo.detach();
+  myservo.detach(); 
 
 }
 
 /* The code in void loop() runs repeatedly forever */ 
 void loop() { 
-
-  setMotorSpeed(100);
-
-  delay(10000);
-
-  while(1);
-
-  // while(1) {
-  //   Serial.println(getDistance(TRIGGER_1, ECHO_1));
-  // }
 	
   double missionLocation;
+  bool bottom;
+  int i;
+
+  getY();
 	
-//  /*
-//  * face the mission site
-//  */
+ /*
+ * face the mission site
+ */
 
 	if (getY() > 1.0) {
 
@@ -61,41 +98,56 @@ void loop() {
   is the exact y coordinate of the center of the site, offset accordingly
   */
 
-  // replace 0.55 with getYMissionLocation()
-
-    missionLocation = getYMissionLocation() + 0.11;
+    missionLocation = Enes100.missionSite.y + 0.11;
 		
 	} else {
 		
     faceDir(NORTH);
 
-    missionLocation = getYMissionLocation() - 0.11;
+    missionLocation = Enes100.missionSite.y - 0.11;
 		
 	}
 
   /*
   * move to the mission site
   */
+	
+	
+	Enes100.updateLocation();
 
   setMotorDir(1, true);
   setMotorDir(2, true);
-	
-	moveToLocation(false, missionLocation);
+  setMotorSpeed(100);
+  delay(5000);
+  setMotorSpeed(0);
+  Enes100.updateLocation();
 
+  // DO MISSION
+  delay(3000);
   // doMission();
 
   setMotorDir(1, false);
   setMotorDir(2, false);
 
-	moveToLocation(false, 1.0);
+  setMotorSpeed(100);
+  delay(5200);
+  setMotorSpeed(0);
 
   setMotorDir(1, true);
   setMotorDir(2, true);
 
-	faceDir(EAST);
-
-  updateLocation();
+	
+	Enes100.updateLocation();
+  delay(100);
+  if (getY() < 1.0) {
+    faceDir(EAST);
+  } else {
+    faceDirRight(EAST);
+  }
   
+
+	Enes100.updateLocation();
+	
 	/* Obstacle avoidance logic */
 
   /*
@@ -104,41 +156,341 @@ void loop() {
   instead of while(1)
   */
 
-	do {
+	while (1) {
 		
-		if (getDistance(TRIGGER_1, ECHO_1) <= 50 || getDistance(TRIGGER_2, ECHO_2) <= 50) {
-			
-      setMotorSpeed(0);
+		Enes100.updateLocation();
 
-			if (getY() < 1.0) {
+    if (Enes100.location.y < 1.0) {
+			bottom = true;
+		} else {
+			bottom = false;
+		}
+		
+		while (1) {
+			Enes100.updateLocation();
+
+			if ((getDistance(TRIGGER_1, ECHO_1) <= 40 || getDistance(TRIGGER_2, ECHO_2) <= 40) && bottom == true) {
+			
+				setMotorSpeed(0);
 				faceDir(NORTH);
-				moveToLocation(false, 1.0);
+				setMotorDir(1, true);
+        setMotorDir(2, true);
+        setMotorSpeed(100);
+				delay(2000);
+        setMotorSpeed(0);
 				faceDir(EAST);
-			} else {
+
+			} else if ((getDistance(TRIGGER_1, ECHO_1) <= 40 || getDistance(TRIGGER_2, ECHO_2) <= 40) && bottom == false) {
+
+				setMotorSpeed(0);
 				faceDir(SOUTH);
-				moveToLocation(false, 1.0);
+				setMotorDir(1, true);
+        setMotorDir(2, true);
+        setMotorSpeed(100);
+				delay(2000);
+        setMotorSpeed(0);
 				faceDir(EAST);
-			}
 
-		} else if (getX() > 3) {
+			} else {
+				break;
+			}
+		}
+    
+    if (Enes100.location.x > 2.7) {
 			
-			if (getY() <= 1.5) {
+			if (Enes100.location.y <= 1.5) {
 				faceDir(NORTH);
-				while (getY() < 1.5) {
+				while (Enes100.location.y < 1.5) {
+					Enes100.updateLocation();
 					setMotorSpeed(100);
 				}
 				faceDir(EAST);
 				setMotorSpeed(100);
 			} else {
-				setMotorSpeed(255);
+				setMotorSpeed(100);
 			}
 			
-		} else {
-			setMotorSpeed(100);
 		}
+    
+    if (Enes100.location.theta > 0 + MARGIN) {
+      faceDir(EAST);
+    } else if (Enes100.location.theta < 0 - MARGIN) {
+      faceDirRight(EAST);
+    }
+
+    setMotorDir(1, true);
+    setMotorDir(2, true);
+		setMotorSpeed(100);
+    delay(2000);
 		
-	} while (1);
+	}
+
+  /*
+  need to add logic here to move OTV to top half of the arena and face the log and go through to end
+  */
 	
+	while(1);  // Circumvent the loop and ensure the above statements only get run once.
+	
+}
+
+/* This is an example function to make both motors drive
+ * at the given power.
+ */
+
+double getX() {
+
+  Enes100.updateLocation();
+
+  return Enes100.location.x;
+
+}
+
+double getY() {
+
+  Enes100.updateLocation();
+
+  return Enes100.location.y;
+
+}
+
+double getHeading() {
+
+  Enes100.updateLocation();
+
+  return Enes100.location.theta;
+
+}
+
+void setMotorSpeed(int PWMspeed) {
+
+  
+  if (PWMspeed == 0) {
+
+    digitalWrite(EN_A, LOW);
+    digitalWrite(EN_B, LOW);
+    return;
+
+  }
+
+  analogWrite(EN_A, PWMspeed);
+  analogWrite(EN_B, PWMspeed);
+
+}
+
+void setMotorDir(int motor, bool forward) {
+
+
+  // left motor
+  if (motor == 1) {
+
+    if (forward) {
+
+      digitalWrite(IN_1, LOW);
+      digitalWrite(IN_2, HIGH);
+
+    } else {
+
+      digitalWrite(IN_1, HIGH);
+      digitalWrite(IN_2, LOW);
+
+    }
+
+  // right motor
+  } if (motor == 2) {
+
+    if (forward) {
+
+      digitalWrite(IN_3, LOW);
+      digitalWrite(IN_4, HIGH);
+
+    } else {
+
+      digitalWrite(IN_3, HIGH);
+      digitalWrite(IN_4, LOW);
+
+    }
+    
+  }
+
+}
+
+void rotateRight(int PWMspeed) {
+
+  setMotorDir(1, true);
+  setMotorDir(2, false);
+  setMotorSpeed(PWMspeed);
+
+}
+
+void rotateLeft(int PWMspeed) {
+
+  setMotorDir(1, false);
+  setMotorDir(2, true);
+  setMotorSpeed(PWMspeed + 24);
+
+}
+
+void faceDir(const double limit) {
+
+  Serial.print("faceDir: ");
+  Serial.println(limit);
+  
+  bool oriented = false;
+
+	do {
+
+    Enes100.println(getHeading());
+    Serial.print("getheading(): ");
+    Serial.println(getHeading());
+
+
+    if ((getHeading() <= limit)) {
+
+      setMotorSpeed(0);
+      oriented = true;
+      Enes100.println("here0");
+      Serial.print("limit0: ");
+      Serial.println(limit);
+
+    }
+	
+		if ((getHeading() < limit - MARGIN) || (getHeading() > limit)) {
+	
+			rotateLeft(120);
+      Enes100.println("here1");
+      Serial.print("limit1: ");
+      Serial.println(limit);
+		
+		}
+
+    delay(50);
+    setMotorSpeed(0);
+
+		
+	} while (!oriented);
+
+  setMotorSpeed(0);
+
+}
+
+void faceDirRight(const double limit) {
+
+  Serial.print("faceDir: ");
+  Serial.println(limit);
+  
+  bool oriented = false;
+
+	do {
+
+    Enes100.println(getHeading());
+    Serial.print("getheading(): ");
+    Serial.println(getHeading());
+
+
+    if (getHeading() >= limit) {
+
+      setMotorSpeed(0);
+      oriented = true;
+      Enes100.println("here0");
+      Serial.print("limit0: ");
+      Serial.println(limit);
+
+    }
+	
+		if ((getHeading() < limit) || (getHeading() > limit + MARGIN)) {
+	
+			rotateRight(120);
+      Enes100.println("here1");
+      Serial.print("limit1: ");
+      Serial.println(limit);
+		
+		}
+
+    delay(50);
+    setMotorSpeed(0);
+
+		
+	} while (!oriented);
+
+  setMotorSpeed(0);
+
+}
+
+void moveToLocation(bool xDir, double limit) {
+
+  setMotorDir(1, true);
+  setMotorDir(2, true);
+	
+	if (xDir) {
+		
+		moveX(limit);	
+		
+	} else {
+		
+		moveY(limit);
+		
+	}
+	
+}
+
+void moveX(double limit) {
+	
+	bool done = false;
+	
+	do {
+		
+		Enes100.updateLocation();
+		
+		if (Enes100.location.x <= limit + MARGIN && Enes100.location.x >= limit - MARGIN) {
+			
+			setMotorSpeed(0);
+			done = true;
+			return;
+			
+		} 
+		
+		setMotorSpeed(90);
+		
+	} while (!done);
+	
+}
+	
+void moveY(double limit) {
+	
+	bool done = false;
+	
+	do {
+		
+		Enes100.updateLocation();
+		
+		if (Enes100.location.y <= limit + MARGIN && Enes100.location.y >= limit - MARGIN) {
+			
+			setMotorSpeed(0);
+			done = true;
+			return;
+			
+		} 
+		
+		setMotorSpeed(90);
+		
+	} while (!done);
+	
+}
+
+int getDistance(int trigger_pin, int echo_pin) {
+
+  // Send ultrasonic pulse
+  digitalWrite(trigger_pin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigger_pin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigger_pin, LOW);
+
+  // Receive echo and calculate distance in cm
+  long duration = pulseIn(echo_pin, HIGH);
+  int distance = (duration * 0.0343) / 2;
+  return distance;
+
 }
 
 void doMission() {
@@ -157,21 +509,27 @@ void doMission() {
     if ( (laserDLeft > 40 && laserDLeft < 60) && (laserDRight > 85 && laserDRight < 110) ) {
       topA++;
       if (topA > 2) {
-        reportTop(1);
+        Enes100.mission(TOPOGRAPHY, TOP_A);
+        topA = 0;
         break;
       }
     } else if ( (laserDLeft > 75 && laserDLeft < 100) && (laserDRight > 65 && laserDRight < 80) ) {
       topB++;
       if (topB > 2) {
-        reportTop(2);
+        Enes100.mission(TOPOGRAPHY, TOP_B);
+        topB = 0;
         break;
       }
     } else if ( (laserDLeft > 40 && laserDLeft < 60) && (laserDRight > 60 && laserDRight < 75) ) {
       topC++;
       if (topC > 2) {
-        reportTop(3);
+        Enes100.mission(TOPOGRAPHY, TOP_C);
+        topC = 0;
         break;
       }
+    } else {
+      Enes100.mission(TOPOGRAPHY, TOP_C);
+      break;
     }
   }
 
@@ -190,7 +548,9 @@ void doMission() {
     num_flames += flame;
   }
 
-  reportFlames(num_flames); 
+  Serial.print("Number of flames: ");
+  Serial.println(num_flames);
+  Enes100.mission(NUM_CANDLES, num_flames);
 
   num_flames = 0;
 
@@ -198,8 +558,7 @@ void doMission() {
 
 
   // SNUFF THE FLAMES
-  myservo.attach(2);
-
+  myservo.attach(2); // attach the servo to our servo object
   myservo.write(105); // rotate the motor (<90 means pull snuffers up, >90 means drop snuffers)
 
   delay(2000); // keep rotating for 5 seconds (5000 milliseconds)
@@ -213,6 +572,7 @@ void doMission() {
   delay(1700);
 
   myservo.write(90); // stop the motor
+  myservo.detach(); 
 
   delay(1000); // stay stopped
 
